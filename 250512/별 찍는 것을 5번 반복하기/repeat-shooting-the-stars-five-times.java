89ca11088a26bdd8e05448599b39f9c1d0b3c055
JinhyeonK <diamondataJH@gmail.com>
2025-05-12 14:24:00 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250512/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,13 @@
+public class Main {  // 클래스 이름 Main
+    public static void main(String[] args)
+ublic class Main {
+    public static void print10Stars() {
+        for(int i=0;i<10;i++){
+            System.out.print("*");
+        System.out.println();
+        }
+        
+        // Please write your code here.
+    }
+}
+}
