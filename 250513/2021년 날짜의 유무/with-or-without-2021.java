f966ab8c0f1e710a657092fb0e384077cfd6617f
JinhyeonK <diamondataJH@gmail.com>
2025-05-13 16:04:26 +0900

    Title: 2021년 날짜의 유무, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250513/2021년 날짜의 유무/with-or-without-2021.java
+++ 250513/2021년 날짜의 유무/with-or-without-2021.java
@@ -4,32 +4,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int d = sc.nextInt();
-        // Please write your code here.
-        if (m<=7&&m%2==1){
-            if(d<=31){
-                System.out.print("Yes");
-            } else{
-                System.out.print("No");
-            }
-        } else if(m<=7&&m%2==0){
-            if (m!=2&&d<=30){
-                System.out.print("Yes");
-            } else{
-                System.out.print("No");
-            }
-        } else if(m>7&&m%2==1){
-            if (d<=30){
-                System.out.print("Yes");
-            } else{
-                System.out.print("No");
-            }
-        }else if(m>7&&m%2==0)
-            {if (d<=31)
-            {System.out.print("Yes");
-            } else{
-                System.out.print("No");
-            }
 
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        // 인덱스:       0  1   2   3   4   5   6   7   8   9  10  11  12
+
+        if (m >= 1 && m <= 12 && d >= 1 && d <= daysInMonth[m]) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
         }
     }
 }
