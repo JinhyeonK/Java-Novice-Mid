ab7cb960d9732a84eff433143aea527521f42671
Jinhyeon Kim <diamondataJH@gmail.com>
2025-06-26 14:51:20 +0900

    Title: DateTime to DateTime, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250626/DateTime to DateTime/datetime-to-datetime.java
+++ 250626/DateTime to DateTime/datetime-to-datetime.java
@@ -1,39 +1,33 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int a,b,c;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
-        int cnt=0;
-        int d,e,f;
-        d=11;
-        e=11;
-        f=11;
+        int a = sc.nextInt(); // year
+        int b = sc.nextInt(); // month
+        int c = sc.nextInt(); // day
+
+        int d = 11, e = 11, f = 11; // reference: 11/11/11 11:11
+        int cnt = 0;
+
         if (a < d || (a == d && b < e) || (a == d && b == e && c < f)) {
             cnt = -1;
         } else {
-        while(true){
-            if( d==a&&e==b&f==c){
-                break;
+            while (true) {
+                if (d == a && e == b && f == c) break;
+                cnt++;
+                f++;
+                if (f == 60) {
+                    f = 0;
+                    e++;
+                }
+                if (e == 24) {
+                    e = 0;
+                    d++;
+                }
             }
-            cnt++;
-            f++;
-            if(f==60){
-                e++;
-                f=0;
+        }
 
-            }
-            if(e==24){
-                d++;
-                e=0;
-
-            }
-
-    System.out.print(cnt);
+        System.out.print(cnt);
     }
-}
-}
 }
